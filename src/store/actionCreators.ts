e76d323c7298a5e11a306a93cbf9a336e4b4f474
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-25 09:59:23 +0530

    functions created for adding Article and removing article.

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,1 +1,25 @@
-export {}
+import * as actionTypes from "./actionTypes";
+
+export function addArticle(article: IArticle) {
+    const action: ArticleAction = {
+        type: actionTypes.ADD_ARTICLE,
+        article
+    }
+    return simulateHttpRequest(action);
+}
+
+export function removeArticle(article: IArticle){
+    const action: ArticleAction = {
+        type: actionTypes.REMOVE_ARTICLE,
+        article
+    }
+    return simulateHttpRequest(action);
+}
+
+export function simulateHttpRequest(action: ArticleAction) {
+    return (dispatch: DisptachType) => {
+        setTimeout(() => {
+            dispatch(action)
+        }, 500)
+    }
+}

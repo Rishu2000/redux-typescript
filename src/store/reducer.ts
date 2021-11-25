678ff8c825e3516a258bf1b9eaa905dbc7d22cc6
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-25 10:10:11 +0530

    we declare an initial state to have some articles to show when the page loads.

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,1 +1,19 @@
-export {}
+import * as actionTypes from "./actionTypes";
+
+const initialState: ArticlesState = {
+    articles : [
+        {
+            id:1,
+            title:"Post 1",
+            body:"Quisque cursus, metus vitae pharetra Nam libero tempore, cum soluta nobis est eligendi"
+        },{
+            id:2,
+            title:"Post 2",
+            body:"2 Quisque cursus, metus vitae pharetra Nam libero tempore, cum soluta nobis est eligendi"
+        },{
+            id:3,
+            title:"Post 3",
+            body:"3 Quisque cursus, metus vitae pharetra Nam libero tempore, cum soluta nobis est eligendi"
+        }
+    ]
+}
